ce453486c9074abdc99d9413e65b5717707a6239
anupam <anupmsingh12@gmail.com>
2023-07-22 18:23:58 +0530

    fixed url

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const route={
-    BASE_URL: 'http://127.0.0.1:4000/',
-    // BASE_URL: 'https://cricketbackend.onrender.com/',
+    // BASE_URL: 'http://127.0.0.1:4000/',
+    BASE_URL: 'https://api.publicprolepoints.in/',
     // ALL_MATCH:"/matchList"
 }
 module.exports = route;
